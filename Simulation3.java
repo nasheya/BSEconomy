89d4d56a3857e71ed2bce2583eab33d0d7ffd3da
Rahman <nr2rt@L1-JPS-G20-03.richmond.edu>
2016-06-01 13:00:25 -0400

    Added some more functionality

--- Simulation3.java
+++ Simulation3.java
@@ -24,12 +24,18 @@ public class Simulation3 {
 	static ArrayList<Double> buyerPrices = new ArrayList<Double>();
 	static ArrayList<Double> sellerPrices = new ArrayList<Double>();
 
+	//list of buyers and sellers
 	static ArrayList<Agent> buyers = new ArrayList<Agent>();
 	static ArrayList<Agent> sellers = new ArrayList<Agent>();
 
+	//costs of the edges
 	static double costCreate;
 	static double costDissolve;
 	static double costMaintain;
+
+	//the number of "turns" a player gets each round, aka the number of connections 
+	//they're allowed to disconnect and connect, total
+	static int turns = 2;
 
 	public Simulation3(int buyersNum, int sellersNum, double c, double d, double m, int maxRounds){
 		costCreate = c;
@@ -38,7 +44,7 @@ public class Simulation3 {
 
 		Matrix a = new Matrix(buyersNum, sellersNum);
 		Matrix x = new Matrix(buyersNum, sellersNum);
-		Matrix y = new Matrix(sellersNum, buyersNum);
+		Matrix y = new Matrix(buyersNum, sellersNum);
 		Matrix z = new Matrix(buyersNum, sellersNum);
 		Matrix w = new Matrix(buyersNum, sellersNum);
 
@@ -59,6 +65,34 @@ public class Simulation3 {
 	}
 	
 	public static void makeConnections(){
+		Matrix costMatrix = new Matrix(buyers.size() + sellers.size(), buyers.size() + sellers.size());
+
+		ArrayList<Agent> total = new ArrayList<Agent>();
+		total.addAll(buyers);
+		//total.addAll(sellers);
+
+		java.util.Collections.shuffle(total);
+
+		for(int i=0; i<total.size(); i++){
+			Agent temp = total.get(i);
+			double[] net;
+
+			if(temp.myType == Agent.Party.BUYER){
+				net = new double[sellers.size()];
+
+				for(int j=0; j<sellers.size(); j++){
+					if(a.get(i,j) == 1){
+						net[j] = connect(temp.myID, sellers.get(j).myID, Agent.Party.BUYER);
+					} else {
+						net[j] = disconnect(temp.myID, sellers.get(j).myID, Agent.Party.BUYER);
+					}	
+				}
+
+				updateCostMatrix(temp, costMatrix, findMaximums(net), net);
+			} else {
+				net = new double[buyers.size()];
+			}
+		}
 
 	}
 
@@ -70,6 +104,48 @@ public class Simulation3 {
 				}
 			}
 		}
+	}
+
+	private static void updateCostMatrix(Agent player, Matrix costMatrix, ArrayList<Integer> maxes, double[] netBenefit){
+		if(player.myType == Agent.Party.BUYER){
+			
+		}
+	}
+
+
+	private static double connect(int buyerID, int sellerID, Agent.Party type){
+		return -2;
+	}
+
+	private static double disconnect(int buyerID, int sellerID, Agent.Party type){
+		return -2;
+	}
+
+
+	private static ArrayList<Integer> findMaximums(double[] arr){
+		ArrayList<Integer> toReturn = new ArrayList<Integer>();
+		int i = 0;
+		int index = 0;
+		double next = -2;
+		
+		while(i<turns && next!=-1){
+			for(int j=0; j<arr.length; j++){
+				if(arr[j]>next && toReturn.indexOf(j) == -1){
+					index = j;
+					next = arr[j];
+				}
+			}
+
+			if(next!=-1){
+				toReturn.add(index);
+			}
+
+			next = -2;
+
+			i++;
+		}
+
+		return toReturn;
 	}
 
 }
