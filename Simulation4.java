2d693df6b27f9b227056d84393d52c8f85e4cc9e
Rahman <nr2rt@L1-JPS-G20-02.richmond.edu>
2016-06-06 16:57:54 -0400

    Simulation 4 underway

---
+++ Simulation4.java
@@ -0,0 +1,53 @@
+import Jama.*;
+import java.util.ArrayList;
+
+public class Simulation4{
+	//represents all the connections between the buyers and sellers
+	Matrix a;
+
+	//represents the amount they are trading
+	Matrix x;
+
+	static ArrayList<Agent> buyers = new ArrayList<Agent>();
+	static ArrayList<Agent> sellers = new ArrayList<Agent>();
+
+	static double costCreate;
+	static double costDissolve;
+
+	public Simulation4(double c, double d, double numBuyers, double numSellers){
+		costCreate = c;
+		costDissolve = d;
+
+		for(int i=0; i<numBuyers; i++){
+			buyers.add(new Agent());
+		}
+
+		for(int i=0; i<numSellers; i++){
+			sellers.add(new Agent());
+		}
+
+		a = new Matrix(numBuyers, numSellers);
+		x = new Matrix(numBuyers + numSellers, numBuyers + numSellers);
+
+		run();
+	}
+
+
+	public static void run(){
+		ArrayList<Agent> buyersTemp = clone(buyers);
+
+		//Agent temp = 
+	}
+
+
+	private static ArrayList clone(ArrayList<T> obj){
+		ArrayList<T> temp = new ArrayList<T>();
+		
+		for(int i=0; i<obj.size(); i++){
+			temp.add(obj.get(i));
+		}
+
+		return temp;
+	}
+
+}
