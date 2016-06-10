3d579316317df32eb5abe63e50b1b354cc1989aa
nasheya <nasheya.rahman@gmail.com>
2016-06-10 16:29:56 -0400

    Moved some files and started our new simulation
    This is the simulation where there are no buyers and sellers but only
    agents and they haggle

--- Agent.java
+++ Agent.java
@@ -2,92 +2,44 @@ import java.util.ArrayList;
 import Jama.*;
 
 public class Agent {
-	public enum Party { BUYER, SELLER }
+	private int myID;
+	private double amtWheat;
+	private double amtCash;
 
-	ArrayList<Integer> myConnections = new ArrayList<Integer>();
-
-	public int myID;
-	double myAmount = 1;
-	public Party myType;
-	public int numConnections;
-	double myBackpack;
-	double costsToPay;
-
-	//This is the connected component number
-	public int ccNum;
 
 	public Agent(){}
 
-	public Agent(Party type, int id){
+	public Agent(int id){
 		myID = id;
-		myType = type;
-		numConnections = 0;
-		myBackpack = 0;
-		costsToPay = 0;
-		ccNum = 0;
+		amtWheat = Math.random();
+		amtCash = 1 - amtWheat;
 	}
 
-
-	/**
-	* Adds the amount to the backpack
-	*/
-	public void addToBackpack(double amt){
-		myBackpack+=amt;
+	public void setWheat(double wheat){
+		amtWheat = wheat;
 	}
 
-
-	/**
-	* Adds to the costs the agent has to pay
-	*/
-	public void addToCosts(double amt){
-		costsToPay+=amt;
+	public void setCash(double cash){
+		amtCash = cash;
 	}
 
-
-	/**
-	* Sets the costs the agent has to pay
-	*/
-	public void resetCosts(){
-		costsToPay = 0;
+	public double getWheat(){
+		return amtWheat;
 	}
 
-
-	/**
-	* Sets the costs the agent has to pay
-	*/
-	public void subtractCosts(){
-		myBackpack-=costsToPay;
+	public double getCash(){
+		return amtCash;
 	}
 
-
-	/**
-	* This method finds if the agent is connected to the other agent given.
-	*/
-	public boolean isConnected(int agentID){
-		return this.bSearch(0, myConnections.size()-1, agentID);
+	public int getID(){
+		return myID;
 	}
 
+	public double getUtility(){
+		return findUtility(amtWheat, amtCash);
+	}
 
-	/**
-	* This method finds the agent connection by a binary search.
-	*/
-	private boolean bSearch(int start, int end, int id){
-		if(start==end){
-			if(myConnections.get(start)==id)
-				return true;
-			return false;
-		}
-
-		int mid  = (start+end)/2;
-
-		if(myConnections.get(mid)==id){
-			return true;
-		} else {
-			if(myConnections.get(mid)>id){
-				return bSearch(start, mid-1, id);
-			} else {
-				return bSearch(mid+1, end, id);
-			}
-		}
+	public double findUtility(double wheat, double cash){
+		return Math.sqrt(wheat)*Math.sqrt(cash);
 	}
 }
--- Simulation.java
+++ Simulation.java
@@ -1,388 +1,111 @@
-/**
-* Author: Nasheya Rahman
-* A simulation concerning a buyer-seller exchange economy as part of Dr. Michael Kerckhove's summer research 2016. 
-*
-* This is attempt 1. The assumptions held are that there is market clearing and the players have no utility for 
-* their good. This attempt is very simplified in that a player decides to connect with another player if 
-* 1/(number of connections expected) - (costs to create)/2 is greater than 0 (meaning you get SOMETHING out of it). 
-* They choose to disconnect if the cost to maintain is higher than the cost to dissolve, and this can be a one-sided 
-* decision (and if it is one-sided, then that person must pay the whole cost, otherwise, they can divide the costs 
-* in half if both decide to dissolve). As for the trading, each player would just divide their good equally among all 
-* their connections, so equal exchange rates were not upheld. After every player got all their goods from their 
-* connections, the costs were subtracted from their "backpack" (where they held all their traded goods). Also, there 
-* are agents randomly added throughout the simulation, according to a probability that was provided earlier. I suppose in 
-* this simulation, it is possible for a player to get a net of negative after their costs are paid because they might've 
-* expected to get more than they really did.
-*/
-
+import Jama.*;
 import java.util.ArrayList;
 import java.util.Random;
-import Jama.*;
 
-public class Simulation {
+public class Simulation{
+
 	static Random rng = new Random();
 
-	//Holds all the connections between buyers and sellers
-	static Matrix a;
+	static ArrayList<Agent> agents = new ArrayList<Agent>();
 
-	//Holds the buyers and sellers
-	static ArrayList<Agent> buyers = new ArrayList<Agent>();
-	static ArrayList<Agent> sellers = new ArrayList<Agent>();
+	static double maxTimeForHaggling = 10;
 
-	//Holds all the possible pair combinations
-	static ArrayList< ArrayList<Integer> > pairs = new ArrayList< ArrayList<Integer> >();
-
-	static double costCreate;
-	static double costDissolve;
-	static double costMaintain;
-	static double prob;
-
-
-	/**
-	* Initalizes all the variables necessary for the simulation and then runs the simulation.
-	* @param c 				Represents the cost of creating an edge
-	* @param d 				Represents the cost of dissolving an edge
-	* @param m 				Represents the cost to maintain an edge
-	* @param buyersNum 		Represents the number of inital buyers in the system
-	* @param sellersNum 	Represents the number of initial sellers in the system
-	* @param maxRounds 		Represents the maximum number of rounds the simulation should execute
-	* @param probAdd		Represents the probability, between 0 and 1, that a buyer or seller would be added to the system
-	*/
-	public Simulation(double c, double d, double m, int buyersNum, int sellersNum, double maxRounds, double probAdd){
-		costCreate = c;
-		costDissolve = d;
-		costMaintain = m;
-
-		if(probAdd>1){
-			prob = 1;
-		} else if(probAdd<0){
-			prob = 0;
-		} else {
-			prob = probAdd;
+	public Simulation(int numAgents){
+		for(int i=0; i<numAgents; i++){
+			//give each agent an id number starting from 0 to n-1
+			agents.add(new Agent(i));
 		}
 
-		a = new Matrix(buyersNum, sellersNum);
+		distributeCashAndWheat();
 
-		//Initialize the list of buyers and sellers
-		for(int i=0; i<a.getRowDimension(); i++){
-			buyers.add(new Agent(Agent.Party.BUYER, i+1));
-		}
-
-		for(int i=0; i<a.getColumnDimension(); i++){
-			sellers.add(new Agent(Agent.Party.SELLER, i+1));
-		}
-
-		//Obtains all the possible pairs
-		for(int i=0; i<a.getRowDimension(); i++){
-			for(int j=0; j<a.getColumnDimension(); j++){
-				ArrayList<Integer> pairTemp = new ArrayList<Integer>();
-				pairTemp.add(i+1);
-				pairTemp.add(j+1);
-				pairs.add(pairTemp);
-			}
-		}
-
-		a.print(1,0);
-
-		//Executes the simulation a certain number of times
-		for(int i=1; i<=maxRounds; i++){
-			System.out.println("Round "+i);
-			playGame();
-		}
+		run();
 	}
 
+	public static void run(){
+		ArrayList<Agent> total = cloneAgents(agents);
 
-	/**
-	* Executes one round of the game by choosing pairs.
-	*/
-	public static void playGame(){
-		addAgent();
+		//boolean equilibrium = false;
 
-		java.util.Collections.shuffle(pairs);
+		while(total.size()>1){
+			//pick an agent index number based on a uniform distribution
+			int index = Random.nextInt(total.size());
 
-		int numPlayersLeft = buyers.size() + sellers.size();
-		boolean[] playerTurns = new boolean[numPlayersLeft];
+			//pick agent here based on an uniform distribution but conditional probability function later
+			int indexPair = Random.nextInt(total.size());
 
-		for(int i=0; i<playerTurns.length; i++){
-			playerTurns[i] = true;
+			//do not let them be the same number
+			while(indexPair == index){
+				indexPair = Random.nextInt(total.size());
+			}
 
-			if(i<buyers.size()){
-				buyers.get(i).resetCosts();
+			Agent one = total.get(index);
+			Agent two = total.get(indexPair);
+
+			if(one.getCash()>two.getCash() || two.getWheat()>one.getWheat()){
+				haggling(one, two);
 			} else {
-				sellers.get(i-buyers.size()).resetCosts();
+				haggling(two, one);
 			}
 		}
+	}
 
-		//Represents the index of the pairs that you are accessing
-		int k = 0;
 
-		while(numPlayersLeft>0){
-			ArrayList<Integer> pairTemp = pairs.get(k);
-			int buyerID = pairTemp.get(0);
-			int sellerID = pairTemp.get(1);
+	public static ArrayList<Agent> cloneAgents(ArrayList<Agent> temp){
+		ArrayList<Agent> toReturn = new ArrayList<Agent>();
 
-			k++;
-
-			//Updating the numPlayersLeft flag
-			if(playerTurns[buyerID-1]==true){
-				playerTurns[buyerID-1] = false;
-				numPlayersLeft--;
-			}
-
-			if(playerTurns[sellerID+buyers.size()-1]==true){
-				playerTurns[sellerID+buyers.size()-1] = false;
-				numPlayersLeft--;
-			}			
-
-			//to connect or disconnect decisions
-			if(a.get(buyerID-1, sellerID-1) == 1){
-				if(shouldDisconnect(buyerID, sellerID)){ //choose to disconnect
-					buyers.get(buyerID-1).numConnections--;
-					sellers.get(sellerID-1).numConnections--;
-					a.set(buyerID-1, sellerID-1, 0);
-				} 
-			} else {
-				if(shouldConnect(buyerID, sellerID)){ //choose to connect
-					buyers.get(buyerID-1).numConnections++;
-					sellers.get(sellerID-1).numConnections++;
-					a.set(buyerID-1, sellerID-1, 1);
-				} 
-			}	
+		for(int i=0; i<temp.size(); i++){
+			toReturn.add(temp.get(i));
 		}
 
-		a.print(1,0);
+		return toReturn;
+	}
 
-		trade();
+
+	public static void haggling(Agent buyer, Agent seller){
+		boolean consensus = false;
+		double t = 0;
+
+		while(!consensus && t < maxTimeForHaggling){
+
+		}
 	}
 
 
 	/**
-	* This method determines if a connected pair should disconnect or not. This was an earlier attempt, but 
-	* upon a closer look, I realized it was incorrect. Still keeping this here for reference purposes though!
+	* This method distributes the amount of wheat and cash such that each agent 
+	* has a total unit of good and that there is only 0.5 * numAgents amount of
+	* cash and wheat in the entire system.
 	*/
-	private static boolean shouldDisconnectWrong(int buyerID, int sellerID){
-		int numConnectB = buyers.get(buyerID-1).numConnections;
-		int numConnectS = sellers.get(sellerID-1).numConnections;
+	private static distributeCashAndWheat(){
+		double totalWheat = 0.5 * agents.size();
+		double distributedWheat = 0;
 
-		double amtS = 1/numConnectB - costMaintain/2;
-		double amtB = 1/numConnectS - costMaintain/2;
-
-		double dS = 0;
-		double dS2 = 0;
-		double dB = 0;
-		double dB2 = 0;
-
-		if(numConnectB>1){
-			dS = 1/(numConnectB-1) - costDissolve;
-			dS2 = 1/(numConnectB-1) - costDissolve/2;
-		}
-
-		if(numConnectS>1){
-			dB = 1/(numConnectS-1) - costDissolve;
-			dB2 = 1/(numConnectS-1) - costDissolve/2;
-		}
-
-		if(dS2>amtS && dB2>amtB){
-			buyers.get(buyerID-1).addToCosts(costDissolve/2);
-			sellers.get(sellerID-1).addToCosts(costDissolve/2);
-		} else if(dS>amtS){
-			sellers.get(sellerID-1).addToCosts(costDissolve);
-		} else if(dB>amtB){
-			buyers.get(buyerID-1).addToCosts(costDissolve);
-		} else {
-			sellers.get(sellerID-1).addToCosts(costMaintain/2);
-			buyers.get(buyerID-1).addToCosts(costMaintain/2);
-		}
-
-		if((amtS<0||amtB<0)||(dS>amtS||dB>amtB)||(dS2>amtS && dB2>amtB)){
-			return true;
-		}
-
-		return false;
-	}
-
-
-	/**
-	* This method determines if a connected pair should disconnect or not.
-	*/	
-	private static boolean shouldDisconnect(int buyerID, int sellerID){
-		int numConnectB = buyers.get(buyerID-1).numConnections;
-		int numConnectS = sellers.get(sellerID-1).numConnections;
-
-		double maintain = costMaintain/2;
-
-		double dS = 0;
-		double dS2 = 0;
-		double dB = 0;
-		double dB2 = 0;
-
-		dB = 1/(numConnectS) + costDissolve;
-		dB2 = 1/(numConnectS) + costDissolve/2;
-		dS = 1/(numConnectB) + costDissolve;
-		dS2 = 1/(numConnectB) + costDissolve/2;
-
-		if(dS2<maintain && dB2<maintain){
-			buyers.get(buyerID-1).addToCosts(costDissolve/2);
-			sellers.get(sellerID-1).addToCosts(costDissolve/2);
-		} else if(dS<maintain){
-			sellers.get(sellerID-1).addToCosts(costDissolve);
-		} else if(dB<maintain){
-			buyers.get(buyerID-1).addToCosts(costDissolve);
-		} else {
-			sellers.get(sellerID-1).addToCosts(maintain);
-			buyers.get(buyerID-1).addToCosts(maintain);
-			return false;
-		}
-
-		return true;
-	}
-
-
-	/**
-	* This method determines if a pair should connect or not.
-	*/
-	private static boolean shouldConnect(int buyerID, int sellerID){
-		int numConnectB = buyers.get(buyerID-1).numConnections;
-		int numConnectS = sellers.get(sellerID-1).numConnections;
-
-		double amtS = 1.0/(numConnectB+1) - costCreate/2;
-		double amtB = 1.0/(numConnectS+1) - costCreate/2;
-
-		buyers.get(buyerID-1).addToCosts(costCreate/2);
-		sellers.get(sellerID-1).addToCosts(costCreate/2);
-
-		if(amtS>0 && amtB>0){
-			return true;
+		//Randomly give each person an amount of wheat, not caring whether or 
+		//not the total is greater or less than 1
+		for(int i=0; i<agents.size(); i++){
+			double wheat = Math.random();
+			agents.get(i).setWheat(wheat);
+			distributedWheat += wheat;
 		}
 		
-		return false;
-	}
+		//adjust the amount of wheat and complement the amount of wheat with cash
+		//so that the total amount is equal to 0.5 * numAgents and each agent has
+		//at most 1 unit of good
+		if(distributedWheat - totalWheat > 0){
+			double amtToEach = (distributedWheat - totalWheat)/agents.size();
 
-
-	/**
-	* Carries out the trading done between the estabished connections.
-	*/
-	private static void trade(){
-		for(int i=0; i<buyers.size(); i++){
-			int connections = buyers.get(i).numConnections;
-
-			if(connections!=0){
-				double amtToEach = 1.0/connections;
-
-				for(int j=0; j<sellers.size(); j++){
-					if(a.get(i,j)==1){
-						sellers.get(j).addToBackpack(amtToEach);
-						System.out.println("Buyer " + (i+1) + " gave " + amtToEach + " to Seller " + (j+1));
-					}
-				}
-			} else {
-				continue;
+			for(int i=0; i<agents.size(); i++){
+				agents.get(i).setWheat(agents.get(i).getWheat() - amtToEach);
+				agents.get(i).setCash(1-agents.get(i).getWheat());
 			}
-		}
+		} else if(distributedWheat - totalWheat < 0){
+			double amtToEach = (totalWheat - distributedWheat)/agents.size();
 
-		for(int i=0; i<sellers.size(); i++){
-			int connections = sellers.get(i).numConnections;
-
-			if(connections!=0){
-				double amtToEach = 1.0/connections;
-
-				for(int j=0; j<buyers.size(); j++){
-					if(a.get(j,i)==1){
-						buyers.get(j).addToBackpack(amtToEach);
-						System.out.println("Seller " + (i+1) + " gave " + amtToEach + " to Buyer " + (j+1));
-					}
-				}
-			} else {
-				continue;
-			}
-		}
-
-		subtractCosts();
-	}
-
-
-	/**
-	* Subtract the costs at the end of the trading round.
-	*/
-	private static void subtractCosts(){
-		for(int i=0; i<sellers.size(); i++){
-			System.out.println("Before for Seller "+(i+1)+": "+sellers.get(i).myBackpack);
-			sellers.get(i).subtractCosts();
-			System.out.println("After for Seller "+(i+1)+": "+sellers.get(i).myBackpack);
-		}
-
-		for(int i=0; i<buyers.size(); i++){
-			System.out.println("Before for Buyer "+(i+1)+": "+buyers.get(i).myBackpack);
-			buyers.get(i).subtractCosts();
-			System.out.println("After for Buyer "+(i+1)+": "+buyers.get(i).myBackpack);
-		}
-	}
-
-
-	/**
-	* Adds an agent or not based on the probability of adding an agent.
-	*/
-	private static void addAgent(){
-		if(rng.nextDouble()<=prob){
-			if(rng.nextDouble()<0.5){
-				buyers.add(new Agent(Agent.Party.BUYER, buyers.size()+1));
-				
-				for(int i=0; i<sellers.size(); i++){
-					ArrayList<Integer> pairTemp = new ArrayList<Integer>();
-					pairTemp.add(buyers.size());
-					pairTemp.add(i+1);
-					pairs.add(pairTemp);
-				}
-
-				a = addMatrixSection(true);
-			} else {
-				sellers.add(new Agent(Agent.Party.SELLER, sellers.size()+1));
-
-				for(int i=0; i<buyers.size(); i++){
-					ArrayList<Integer> pairTemp = new ArrayList<Integer>();
-					pairTemp.add(i+1);
-					pairTemp.add(sellers.size());
-					pairs.add(pairTemp);
-				}
-
-				a = addMatrixSection(false);
+			for(int i=0; i<agents.size(); i++){
+				agents.get(i).setWheat(agents.get(i).getWheat() + amtToEach);
+				agents.get(i).setCash(1-agents.get(i).getWheat());
 			}
 		}
 	}
-
-
-	/**
-	* Resizes the matrix based on whether or not you are adding a buyer or seller.
-	*/
-	private static Matrix addMatrixSection(boolean buyer){
-		Matrix temp;
-
-		if(buyer){
-			temp = new Matrix(a.getRowDimension()+1, a.getColumnDimension());
-
-			for(int i=0; i<=a.getRowDimension(); i++){
-				for(int j=0; j<a.getColumnDimension(); j++){
-					if(i!=a.getRowDimension()){
-						temp.set(i, j, a.get(i,j));
-					} else {
-						temp.set(i,j,0);
-					}
-				}
-			}
-		} else {
-			temp = new Matrix(a.getRowDimension(), a.getColumnDimension()+1);
-
-			for(int i=0; i<a.getRowDimension(); i++){
-				for(int j=0; j<=a.getColumnDimension(); j++){
-					if(j!=a.getColumnDimension()){
-						temp.set(i, j, a.get(i,j));
-					} else {
-						temp.set(i,j,0);
-					}
-				}
-			}
-		}
-
-		return temp;
-	}
+	
 }
