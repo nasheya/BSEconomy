e2bdb768b2aa56c66a37fea7f92b419092093567
Nasheya Rahman <nasheya.rahman@gmail.com>
2016-07-05 10:17:33 -0400

    Ran simulations

--- MatlabScripts/matlabScript.m
+++ MatlabScripts/matlabScript.m
@@ -1,10 +1,12 @@
+n=10;
+
 % % % Total Utility Graph % % % 
 au = importdata('../../Desktop/BSEconomy/DataFiles/AfterUtility.txt');
 bu = importdata('../../Desktop/BSEconomy/DataFiles/BeforeUtility.txt');
 histogram(bu,50);
 hold on;
 histogram(au,100);
-title('Before and After Utility Distribution of 50 Simulations with a 25% Tax (50 Players, 500 Rounds)');
+title('Before and After Utility Distribution of 50 Simulations with 25% Tax (10 Players, 500 Rounds)');
 xlabel('Total Utility');
 ylabel('Number of Players');
 legend('Before','After');
@@ -15,7 +17,7 @@ close();
 % % % Total Amount Graph % % % 
 ta = importdata('../../Desktop/BSEconomy/DataFiles/TotalAmount.txt');
 histogram(ta,100);
-title('Total Amount Distribution of 50 Simulations with a 25% Tax (50 Players, 500 Rounds)');
+title('Total Amount Distribution of 50 Simulations with 25% Tax (10 Players, 500 Rounds)');
 xlabel('Total Amount');
 ylabel('Number of Players');
 savefig('../../Desktop/BSEconomy/PlotFiles/Amount.fig');
@@ -25,24 +27,24 @@ close();
 % % % Cash Wheat Graph % % % 
 a = importdata('../../Desktop/BSEconomy/DataFiles/AfterCashWheat.txt');
 b = importdata('../../Desktop/BSEconomy/DataFiles/BeforeCashWheat.txt');
-plot(b(1:2500,1:1),b(1:2500,2:2));
+plot(b(1:n*50,1:1),b(1:n*50,2:2));
 hold on;
-plot(a(1:2500,1:1),a(1:2500,2:2),'.');
+plot(a(1:n*50,1:1),a(1:n*50,2:2),'.');
 xlabel('Cash');
 ylabel('Wheat');
-title('Cash and Wheat Distribution of 50 Simulations with a 25% Tax (50 Players, 500 Rounds)');
+title('Cash and Wheat Distribution of 50 Simulations with 25% Tax (10 Players, 500 Rounds)');
 legend('Before','After');
 savefig('../../Desktop/BSEconomy/PlotFiles/CashWheat.fig');
 print('../../Desktop/BSEconomy/Graphs/CashWheat.png','-dpng');
 close();
 
-% % % Government Graph % % %
-govt = importdata('../../Desktop/BSEconomy/DataFiles/Govt.txt');
-plot(govt(1:50,1:1),govt(1:50,2:2), '.');
-hold on;
-xlabel('Cash');
-ylabel('Wheat');
-title('Government Tax Distribution of 50 Simulations with a 25% Tax (50 Players, 500 Rounds)');
-savefig('../../Desktop/BSEconomy/PlotFiles/Govt.fig');
-print('../../Desktop/BSEconomy/Graphs/Govt.png','-dpng');
-close();
+% % % % Government Graph % % %
+% govt = importdata('../../Desktop/BSEconomy/DataFiles/Govt.txt');
+% plot(govt(1:50,1:1),govt(1:50,2:2), '.');
+% hold on;
+% xlabel('Cash');
+% ylabel('Wheat');
+% title('Government Tax Distribution of 50 Simulations with a 25% Tax (10 Players, 500 Rounds)');
+% savefig('../../Desktop/BSEconomy/PlotFiles/Govt.fig');
+% print('../../Desktop/BSEconomy/Graphs/Govt.png','-dpng');
+% close();
--- MatlabScripts/ratesScript.m
+++ MatlabScripts/ratesScript.m
@@ -6,24 +6,24 @@ for n=1:50
     rates = importdata(strcat('../../Desktop/BSEconomy/DataFiles/Rates/Rates',num2str(n),'.txt'));
     h=size(rates,1);
     h1 = max(h1,h);
-    plot(rates(1:h, 1:1),rates(1:h,2:2));
-    hold on;
+    if h>=1
+        plot(rates(1:h, 1:1),rates(1:h,2:2));
+        hold on;
 
-    if h<=100
-        ans1=mean(rates(1:h,2:2));
-    elseif h<=200
-        ans1=mean(rates((h-100):h,2:2));
-    else
-        ans1=mean(rates((h-200):h, 2:2));
+        if h<=20
+            ans1=mean(rates(1:h,2:2));
+        else
+            ans1=mean(rates((h-20):h, 2:2));
+        end
+
+        sum = sum + ans1;
     end
-
-    sum = sum + ans1;
 end
 
 ans2 = sum/50;
 xl=xlim;
 
-if xl(1,2)-h1<20
+if xl(1,2)-h1<10
     val=xl(1,2);
 else 
     val=h1;
@@ -32,11 +32,15 @@ end
 x = linspace(1,h1);
 plot(x,ones(size(x))*ans2, 'LineWidth',5);
 txt1 = num2str(ans2);
-text(val+5,ans2,txt1);
+text(val+0.5,ans2,txt1);
 
-ylim([0,5]);
+yl=ylim;
 
-title('Transaction Rate Convergence from 50 Simulations with a 25% Tax (50 Agents, 500 Rounds)');
+if yl(1,2)>5
+    ylim([0,5]);
+end
+
+title('Transaction Rate Convergence from 50 Simulations with 25% Tax (10 Agents, 500 Rounds)');
 xlabel('Time');
 ylabel('Transaction Rate (Cash/Wheat)');
 
